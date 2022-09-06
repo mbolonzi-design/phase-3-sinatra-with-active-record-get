07a1103e24bf71c4cc61d74bbec1919f9e44343b
mbolonzi-design <mbolonzi.design@gmail.com>
2022-09-06 13:37:22 +0300

    commit tree 63840530b9642f6ec86052251ad7c8b9ae256c91

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    #get all games fro the database
+    games = Game.all.order(:title).limit(7) 
+    #return a JSON with an array of games data
+    games.to_json
+  end
+  #dynamic route
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
